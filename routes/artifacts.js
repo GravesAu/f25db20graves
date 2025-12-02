384c754397cc825b2971455a4e634c1d3267a5dd
August Graves <124806122+GravesAu@users.noreply.github.com>
2025-12-02 14:21:42 -0600

    loging and register up and going

--- routes/artifacts.js
+++ routes/artifacts.js
@@ -1,11 +1,24 @@
 var express = require('express');
 var router = express.Router();
+
+// A little function to check if we have an authorized user and continue on
+// or redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
 var artifact_controller = require('../controllers/artifact');
 
 // Pug Pages
 router.get('/', artifact_controller.artifact_view_all_Page); // All artifacts
 router.get('/create', artifact_controller.artifact_create_Page); // Create form
-router.get('/update', artifact_controller.artifact_update_Page); // Update form
+
+/* GET update artifact page (protected) */
+router.get('/update', secured, artifact_controller.artifact_update_Page);
+
 router.get('/detail', artifact_controller.artifact_view_one_Page); // Detail page
 router.get('/delete', artifact_controller.artifact_delete_Page); // Delete page
 
